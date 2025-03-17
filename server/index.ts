096a2bc6c99c73930fa2e3b906958aef13bbc036
nandhu-navneeth <nandhunavneeth9@gmail.com>
2025-03-18 03:53:21 +0530

    Fix GitHub Pages deployment configuration

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -53,7 +54,10 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    app.use("/NavneethKManoj", express.static("dist"));
+    app.get("/NavneethKManoj/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../dist/index.html"));
+    });
   }
 
   // ALWAYS serve the app on port 3000
